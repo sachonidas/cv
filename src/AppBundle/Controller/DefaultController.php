18e8d5d2e8ece126cc467aa4a1412779e04b5a24
l.arancibia <lsachaabazan@gmail.com>
2017-07-19 14:46:25 +0200

    menu superior

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,14 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("/datos-personales", name="datosPersonales")
+     */
+    public function datosPresonalesAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('personales/personales.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
